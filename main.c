5482eef7a41905fddbf08638d42efb6f7ec763bf
Juliadsa <juliadsa@hotmail.com>
2019-11-12 21:28:55 -0300

    juliaporraaaa

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "banco.h"
 #include "gerenciaClientes.h"
 
-//OI JULIA SOU EU O GUILHERME CARAIIIIIII ALEK PARA DE RIR PORRA
+//OI JULIA SOU EU O GUILHERME CARAIIIIIII ALEK PARA DE RIR PORRA CARAIIII
 
 void menuPrincipal(){
     printf("=== Menu do Banco ===\n");
